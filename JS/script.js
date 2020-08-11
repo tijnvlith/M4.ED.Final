3b878865abaebdb2475fb9446c3a5030b17f2ae0
tijnvlith <66628263+tijnvlith@users.noreply.github.com>
2020-08-12 00:25:46 +0200

    Add files via upload

--- JS/script.js
+++ JS/script.js
@@ -1,46 +1,48 @@
+
+//Standard output
+
 var wrapper = document.querySelector('#wrapper')
 
-function btnClick1() {
+wrapper.innerHTML = '<ul>' + movies.map(function (movies) {
+    return '<li><a href="https://www.imdb.com/title/' + movies["imdbID"] + '/?ref_=nv_sr_srsg_0"><img src="' + movies["Poster"] + '" alt=""></img></a></li>';
+}).join('') + '</ul>';
 
-    let newMovies = movies.filter(function (el) { return el.Year >= 2014; });
+//Output filter
 
-    wrapper.innerHTML = '<ul>' + newMovies.map(function (movies) {
-        return '<li><img src="' + movies["Poster"] + '" alt=""></img></li>';
-    }).join('') + '</ul>';
+let newMovies = movies.filter(function (el) { return el.Year >= 2014; });
+
+let avnMovies = movies.filter(item => item.Title.indexOf('Avengers') !== -1);
+
+let xMovies = movies.filter(item => item.Title.indexOf('X-Men') !== -1);
+
+let prsMovies = movies.filter(item => item.Title.indexOf('Princess') !== -1);
+
+let bmMovies = movies.filter(item => item.Title.indexOf('Batman') !== -1);
+
+var rad = document.getElementsByName("drone");
+
+//Output decider
+
+for (var i = 0; i < rad.length; i++) {
+    rad[i].addEventListener('click', function () {
+        if (this.value == 1) {
+            movieOutput(newMovies)
+        } else if (this.value == 2) {
+            movieOutput(avnMovies)
+        } else if (this.value == 3) {
+            movieOutput(xMovies)
+        } else if (this.value == 4) {
+            movieOutput(prsMovies)
+        } else if (this.value == 5) {
+            movieOutput(bmMovies)
+        }
+    });
 }
 
-function btnClick2() {
+//Output function
 
-    let avnMovies = movies.filter(item => item.Title.indexOf('Avengers') !== -1);
-
-    wrapper.innerHTML = '<ul>' + avnMovies.map(function (movies) {
-        return '<li><img src="' + movies["Poster"] + '" alt=""></img></li>';
-    }).join('') + '</ul>';
-}
-
-function btnClick3() {
-
-    let xMovies = movies.filter(item => item.Title.indexOf('X-Men') !== -1);
-
-    wrapper.innerHTML = '<ul>' + xMovies.map(function (movies) {
-        return '<li><img src="' + movies["Poster"] + '" alt=""></img></li>';
-    }).join('') + '</ul>';
-}
-
-function btnClick4() {
-
-    let prsMovies = movies.filter(item => item.Title.indexOf('Princess') !== -1);
-
-    wrapper.innerHTML = '<ul>' + prsMovies.map(function (movies) {
-        return '<li><img src="' + movies["Poster"] + '" alt=""></img></li>';
-    }).join('') + '</ul>';
-}
-
-function btnClick5() {
-
-    let bmMovies = movies.filter(item => item.Title.indexOf('Batman') !== -1);
-
-    wrapper.innerHTML = '<ul>' + bmMovies.map(function (movies) {
-        return '<li><img src="' + movies["Poster"] + '" alt=""></img></li>';
+function movieOutput(name) {
+    wrapper.innerHTML = '<ul>' + name.map(function (movies) {
+        return '<li><a href="https://www.imdb.com/title/' + movies["imdbID"] + '/?ref_=nv_sr_srsg_0"><img src="' + movies["Poster"] + '" alt=""></img></a></li>';
     }).join('') + '</ul>';
 }
